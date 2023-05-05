9ef2beaa16645476f84ca48ef5bbec9c9736a477
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-06 02:49:30 +0430

    Prototype learning mnist

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 use learst::funcs::*;
-use learst::graph::{Graph, TensorId};
+use learst::graph::Graph;
 use learst::optimizer::NaiveOptimizer;
-use learst::tensor::{Tensor, TensorMutOps, TensorOps};
-use learst::*;
+use learst::tensor::{Tensor, TensorMutOps};
 use rand::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn mse(g: &mut Graph, out: TensorId, target: TensorId) -> TensorId {
-    let diff = g.call(Sub::new(), &[out, target]);
-    let diff_sqr = g.call(Square::new(), &[diff]);
-    diff_sqr
-}
-
 fn mnist_images() -> std::io::Result<(Tensor<f32>, Tensor<u32>)> {
     let mut img_file = File::open("train-images-idx3-ubyte")?;
     let mut img_bytes = Vec::new();
-    img_file.read_to_end(&mut img_bytes);
+    img_file.read_to_end(&mut img_bytes)?;
     let mut label_file = File::open("train-labels-idx1-ubyte")?;
     let mut label_bytes = Vec::new();
-    label_file.read_to_end(&mut label_bytes);
+    label_file.read_to_end(&mut label_bytes)?;
 
     let num_img_samples = u32::from_be_bytes(img_bytes[4..8].try_into().unwrap()) as usize;
     let num_label_samples = u32::from_be_bytes(label_bytes[4..8].try_into().unwrap()) as usize;
@@ -46,25 +39,28 @@ fn main() {
     let mut rng = thread_rng();
     let mut g = Graph::new();
 
-    let xs = Tensor::<f32>::rand(&mut rng, &[10, 2]);
-    let ys = xs.map(1, |l| Tensor::<u32>::scalar(0));
-    //print
+    let (xs, ys) = mnist_images().unwrap();
 
     let samples = g.alloc(xs);
 
-    let lin1 = g.alloc(Tensor::<f32>::rand(&mut rng, &[2, 2]));
-    let lin1_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[2]));
-    let lin2 = g.alloc(Tensor::<f32>::rand(&mut rng, &[2, 2]));
-    let lin2_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[2]));
+    let lin1 = g.alloc(Tensor::<f32>::rand(&mut rng, &[784, 200]));
+    let lin1_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[200]));
+    let lin2 = g.alloc(Tensor::<f32>::rand(&mut rng, &[200, 100]));
+    let lin2_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[100]));
+    let lin3 = g.alloc(Tensor::<f32>::rand(&mut rng, &[100, 10]));
+    let lin3_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[10]));
 
     let out1 = g.call(MatMul::new(), &[samples, lin1]);
     let out1_bias = g.call(Add::new(), &[out1, lin1_bias]);
     let out1_bias_sigm = g.call(Sigmoid::new(), &[out1_bias]);
     let out2 = g.call(MatMul::new(), &[out1_bias_sigm, lin2]);
     let out2_bias = g.call(Add::new(), &[out2, lin2_bias]);
-    let out = g.call(Softmax::new(), &[out2_bias]);
+    let out2_bias_sigm = g.call(Sigmoid::new(), &[out2_bias]);
+    let out3 = g.call(MatMul::new(), &[out2_bias_sigm, lin3]);
+    let out3_bias = g.call(Add::new(), &[out3, lin3_bias]);
+    let out3_bias_sigm = g.call(Sigmoid::new(), &[out3_bias]);
 
-    let error = g.call(CrossEntropy::new(2, ys), &[out]);
+    let error = g.call(CrossEntropy::new(10, ys), &[out3_bias_sigm]);
     //let squared_error = g.call(Square::new(), &[error]);
     let mean_error = g.call(Mean::new(), &[error]);
 
@@ -76,7 +72,9 @@ fn main() {
         println!("{:?}", g.get(mean_error));
         g.optimize(
             &mut opt,
-            &[lin1, lin2, lin1_bias, lin2_bias].into_iter().collect(),
+            &[lin1, lin2, lin3, lin1_bias, lin2_bias, lin3_bias]
+                .into_iter()
+                .collect(),
         );
     }
 }
