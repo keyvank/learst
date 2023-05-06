020367e874e60647b8617e9c81166a9d57e8f7e7
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-07 02:40:34 +0430

    Learn xor without categorial cross-entropy

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,9 @@ fn mnist_images() -> std::io::Result<(Tensor<f32>, Tensor<u32>)> {
     Ok((images, labels))
 }
 
-fn xor_dataset() -> (Tensor<f32>, Tensor<u32>) {
+fn xor_dataset() -> (Tensor<f32>, Tensor<f32>) {
     let xs = Tensor::<f32>::raw(&[4, 2], vec![0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]);
-    let ys = Tensor::<u32>::raw(&[4], vec![1, 1, 1, 1]);
+    let ys = Tensor::<f32>::raw(&[4, 1], vec![0.0, 1.0, 1.0, 0.5]);
     (xs, ys)
 }
 
@@ -48,13 +48,16 @@ fn main() {
     let (xs, ys) = xor_dataset();
 
     let samples = g.alloc(xs);
+    let expected = g.alloc(ys);
 
     let lin1 = g.alloc(Tensor::<f32>::rand(&mut rng, &[2, 10]));
     let lin1_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[10]));
     let lin2 = g.alloc(Tensor::<f32>::rand(&mut rng, &[10, 20]));
     let lin2_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[20]));
-    let lin3 = g.alloc(Tensor::<f32>::rand(&mut rng, &[20, 2]));
-    let lin3_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[2]));
+    let lin3 = g.alloc(Tensor::<f32>::rand(&mut rng, &[20, 20]));
+    let lin3_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[20]));
+    let lin4 = g.alloc(Tensor::<f32>::rand(&mut rng, &[20, 1]));
+    let lin4_bias = g.alloc(Tensor::<f32>::rand(&mut rng, &[1]));
 
     let out1 = g.call(MatMul::new(), &[samples, lin1]);
     let out1_bias = g.call(Add::new(), &[out1, lin1_bias]);
@@ -64,23 +67,29 @@ fn main() {
     let out2_bias_sigm = g.call(Sigmoid::new(), &[out2_bias]);
     let out3 = g.call(MatMul::new(), &[out2_bias_sigm, lin3]);
     let out3_bias = g.call(Add::new(), &[out3, lin3_bias]);
-    let out3_bias_sigm = g.call(Softmax::new(), &[out3_bias]);
+    let out3_bias_sigm = g.call(Sigmoid::new(), &[out3_bias]);
+    let out4 = g.call(MatMul::new(), &[out3_bias_sigm, lin4]);
+    let out4_bias = g.call(Add::new(), &[out4, lin4_bias]);
+    let out4_bias_sigm = g.call(Sigmoid::new(), &[out4_bias]);
 
-    let error = g.call(CrossEntropy::new(2, ys), &[out3_bias_sigm]);
-    //let squared_error = g.call(Square::new(), &[error]);
-    let mean_error = g.call(Mean::new(), &[error]);
+    let error = g.call(Sub::new(), &[out4_bias_sigm, expected]);
+    let squared_error = g.call(Square::new(), &[error]);
+    let mean_error = g.call(Mean::new(), &[squared_error]);
 
-    let mut opt = NaiveOptimizer::new(0.001);
+    let mut opt = NaiveOptimizer::new(1.);
     loop {
         g.forward();
         g.zero_grad();
         g.backward_all(mean_error);
         println!("{:?}", g.get(mean_error));
+        println!("{:?}", g.get(out4_bias_sigm));
         g.optimize(
             &mut opt,
-            &[lin1, lin2, lin3, lin1_bias, lin2_bias, lin3_bias]
-                .into_iter()
-                .collect(),
+            &[
+                lin1, lin2, lin3, lin4, lin1_bias, lin2_bias, lin3_bias, lin4_bias,
+            ]
+            .into_iter()
+            .collect(),
         );
     }
 }
