a60bb8999146a34bc9b477374fa084b7b4aedb8c
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-19 03:01:24 +0430

    First impl of dropout layer

--- src/funcs/dropout.rs
+++ src/funcs/dropout.rs
@@ -1,24 +1,32 @@
-use super::{Function, Tensor};
+use super::{Function, Tensor, TensorOps};
 
 pub struct Dropout {
+    mask: Tensor<f32>,
     rate: f32,
 }
 impl Dropout {
     pub fn new(rate: f32) -> Box<dyn Function> {
-        Box::new(Self { rate })
+        Box::new(Self {
+            rate,
+            mask: Tensor::scalar(0.),
+        })
     }
 }
 
 impl Function for Dropout {
-    fn run(&mut self, _inps: &[&Tensor<f32>]) -> Tensor<f32> {
-        todo!()
+    fn run(&mut self, inps: &[&Tensor<f32>]) -> Tensor<f32> {
+        let mut rng = rand::thread_rng();
+        let rnd = Tensor::<f32>::rand(&mut rng, inps[0].shape());
+        let scale = 1. / (1. - self.rate);
+        self.mask = rnd.map_values(|v| if v > self.rate { scale } else { 0. });
+        inps[0] * &self.mask
     }
     fn grad(
         &self,
         _inps: &[&Tensor<f32>],
         _out: &Tensor<f32>,
-        _out_grad: &Tensor<f32>,
+        out_grad: &Tensor<f32>,
     ) -> Vec<Tensor<f32>> {
-        todo!()
+        vec![out_grad * &self.mask]
     }
 }
